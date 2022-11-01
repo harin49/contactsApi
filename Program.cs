bd89cbffc905575fed7238fad661731ff0a77fe0
Hari Narayanan <harinarayanan@Haris-MacBook-Pro.local>
2022-11-01 18:24:13 +0530

    [Hari] Context in memory db to services and update dependencies for jsonPatch usage

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
+using contactsApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddNewtonsoftJson();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ContactsApiDbContext>(options => options.UseInMemoryDatabase("ContactsDb"));
 
 var app = builder.Build();
 
